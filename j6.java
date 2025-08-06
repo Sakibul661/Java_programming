fe138790b278586fe46c0e824e8e454bdf461e7b
Md. Sakibul Islam <661sakibulislam@gmail.com>
2025-08-06 16:43:17 +0600

    Add files via upload

---
+++ j6.java
@@ -0,0 +1,5 @@
+public class j6{
+  public static void main(String[] args) {
+    System.out.println(3 + 3);
+  }
+}
---
+++ j9.java
@@ -0,0 +1,5 @@
+public class j9 {
+  public static void main(String[] args) {
+    System.out.println("Hello World"); // This is a comment
+  }
+}
